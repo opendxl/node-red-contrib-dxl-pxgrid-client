5466b4ebc19304f1d04751d68c15f2caa9d7d2a8
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-05-07 14:12:22 -0700

    Initial implementation

---
+++ nodes/dxl-ise-anc-apply-endpoint-policy-by-mac.js
@@ -0,0 +1,83 @@
+'use strict'
+
+var dxl = require('@opendxl/dxl-client')
+var MessageUtils = require('@opendxl/dxl-bootstrap').MessageUtils
+var util = require('../lib/util')
+
+var ISE_EVENT_ANC_APPLY_ENDPOINT_POLICY_BY_MAC_TOPIC = util.ISE_ANC_PREFIX +
+  'applyendpointpolicybymac'
+
+module.exports = function (RED) {
+  function IseAncApplyEndpointPolicyByMacNode (nodeConfig) {
+    RED.nodes.createNode(this, nodeConfig)
+
+    this._policy = nodeConfig.policy
+    /**
+     * Handle to the DXL client node used to make requests to the DXL fabric.
+     * @type {Client}
+     * @private
+     */
+    this._client = RED.nodes.getNode(nodeConfig.client)
+
+    var node = this
+
+    this.status({
+      fill: 'red',
+      shape: 'ring',
+      text: 'node-red:common.status.disconnected'
+    })
+
+    if (this._client) {
+      this._client.registerUserNode(this)
+      this.on('input', function (msg) {
+        var policy = node._policy || msg.policy
+        if (!msg.payload) {
+          this.error(
+            'Unable to send request, mac address not available in payload.')
+        } else if (!policy) {
+          this.error('Unable to send request, no policy available')
+        } else if (!node._client.connected) {
+          this.error('Unable to send request, not connected')
+        } else {
+          var request = new dxl.Request(
+            ISE_EVENT_ANC_APPLY_ENDPOINT_POLICY_BY_MAC_TOPIC)
+          MessageUtils.objectToJsonPayload(request, {
+            mac: msg.payload,
+            policyName: policy
+          })
+          this._client.asyncRequest(request,
+            function (error, response) {
+              if (error) {
+                node.error(error.message, msg)
+              } else {
+                try {
+                  msg.payload = MessageUtils.jsonPayloadToObject(response)
+                  node.send(msg)
+                } catch (e) {
+                  node.error(
+                    'Error converting response to object. Error: ' +
+                    e.message + ', Payload: ' + response.payload, msg)
+                }
+              }
+            }
+          )
+        }
+      })
+      this.on('close', function (done) {
+        node._client.unregisterUserNode(node, done)
+      })
+      if (this._client.connected) {
+        this.status({
+          fill: 'green',
+          shape: 'dot',
+          text: 'node-red:common.status.connected'
+        })
+      }
+    } else {
+      this.error('Missing client configuration')
+    }
+  }
+
+  RED.nodes.registerType('dxl-ise-anc-apply-endpoint-policy-by-mac',
+    IseAncApplyEndpointPolicyByMacNode)
+}
---
+++ nodes/dxl-ise-anc-apply-endpoint-policy-in.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var bootstrap = require('@opendxl/dxl-bootstrap')
+var MessageUtils = bootstrap.MessageUtils
+var util = require('../lib/util')
+
+var ISE_EVENT_ANC_APPLY_ENDPOINT_POLICY_TOPIC = util.ISE_ANC_EVENT_PREFIX +
+  'applyendpointpolicy'
+
+module.exports = function (RED) {
+  function IseAncApplyEndpointPolicyInNode (nodeConfig) {
+    RED.nodes.createNode(this, nodeConfig)
+
+    /**
+     * Handle to the DXL client node used to make requests to the DXL fabric.
+     * @type {Client}
+     * @private
+     */
+    this._client = RED.nodes.getNode(nodeConfig.client)
+
+    var node = this
+
+    this.status({
+      fill: 'red',
+      shape: 'ring',
+      text: 'node-red:common.status.disconnected'
+    })
+
+    if (this._client) {
+      this._client.registerUserNode(this)
+      var callback = function (event) {
+        try {
+          node.send({payload: MessageUtils.jsonPayloadToObject(event)})
+        } catch (e) {
+          node.error('Error converting event to json. Error: ' + e.message +
+            ', Payload: ' + event.payload, {payload: event.payload})
+        }
+      }
+      this._client.addEventCallback(ISE_EVENT_ANC_APPLY_ENDPOINT_POLICY_TOPIC,
+        callback)
+      this.on('close', function (done) {
+        node._client.removeEventCallback(
+          ISE_EVENT_ANC_APPLY_ENDPOINT_POLICY_TOPIC, callback)
+        node._client.unregisterUserNode(node, done)
+      })
+      if (this._client.connected) {
+        this.status({
+          fill: 'green',
+          shape: 'dot',
+          text: 'node-red:common.status.connected'
+        })
+      }
+    } else {
+      this.error('Missing client configuration')
+    }
+  }
+
+  RED.nodes.registerType('dxl-ise-anc-apply-endpoint-policy in',
+    IseAncApplyEndpointPolicyInNode)
+}
